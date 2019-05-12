6dde69a89e763f1e0575996c0ebe6ff2d6841918
Sandy <thecsw@ku.edu>
2019-05-12 16:53:02 -0500

    Updated the main script

--- main.go
+++ main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"fmt"
-
 	"./goraw"
 )
 
 func main() {
-	cred := goraw.Credentials{
-		"PMQYCRTBZq6qHw",
-		"oLWXPkWF80zF8KnejyGjvZB_6VE",
-		"HiveWriting_bot",
-		"0p3244wGerMIYDZD",
-		"HiveWriting_bot Ubuntu 16.04 (Reddit Hive Mind is writing a poem)",
-	}
+	cred := goraw.ReadCreds("login.conf")
 	// It will handle aute refreshing the tokens
 	r := goraw.Init(cred)
 	fmt.Println(r)
 
 	fmt.Println(r.Me())
 	fmt.Println(r.GetUser("thecsw"))
+	post := r.Submit("mypost", "hello, world", "memeinvestor_test")
+	comment := post.Comment(r, "My First Comment")
+	reply := comment.Reply(r, "My Reply to the First Comment")
+	fmt.Println(reply)
 }
