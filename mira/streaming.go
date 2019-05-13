f17e752987775b1b250808b3c71632ae04fdc7b0
Sandy <thecsw@ku.edu>
2019-05-13 11:26:58 -0500

    Configured the reading of comment replies

--- mira/streaming.go
+++ mira/streaming.go
@@ -11,17 +11,19 @@ const (
 // c is the channel with all unread messages
 // stop is the channal to stop the stream. Do stop <- true to stop the loop
 func (r *Reddit) StreamCommentReplies() (<-chan ListingDataChildren, chan bool) {
-	c := make(chan ListingDataChildren, 50)
+	c := make(chan ListingDataChildren, 25)
 	stop := make(chan bool, 1)
 	go func() {
 		for {
 			stop <- false
 			un, _ := r.ListUnreadMessages()
 			for _, v := range un.GetMessages() {
+				// Only process comment replies and
+				// mark them as read. 
 				if v.IsCommentReply() {
 					c <- v
+					r.ReadMessage(v.GetId())
 				}
-				r.ReadMessage(v.GetId())
 			}
 			time.Sleep(CommentListInterval * time.Second)
 			if <-stop {
