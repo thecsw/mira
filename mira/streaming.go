d442c9dc5db8845c0adb1d78ab49cf4980e2dd3f
Sandy <thecsw@ku.edu>
2019-05-13 10:55:22 -0500

    Updated the commentreply stream. Now we can stop it

--- mira/streaming.go
+++ mira/streaming.go
@@ -4,10 +4,18 @@ import (
 	"time"
 )
 
-func (r *Reddit) StreamCommentReplies() <-chan ListingDataChildren {
+const (
+	CommentListInterval = 8
+)
+
+// c is the channel with all unread messages
+// stop is the channal to stop the stream. Do stop <- true to stop the loop
+func (r *Reddit) StreamCommentReplies() (<-chan ListingDataChildren, chan bool) {
 	c := make(chan ListingDataChildren, 50)
+	stop := make(chan bool, 1)
 	go func() {
 		for {
+			stop <- false
 			un, _ := r.ListUnreadMessages()
 			for _, v := range un.GetMessages() {
 				if v.IsCommentReply() {
@@ -15,8 +23,11 @@ func (r *Reddit) StreamCommentReplies() <-chan ListingDataChildren {
 				}
 				r.ReadMessage(v.GetId())
 			}
-			time.Sleep(5 * time.Second)
+			time.Sleep(CommentListInterval * time.Second)
+			if <-stop {
+				return
+			}
 		}
 	}()
-	return c
+	return c, stop
 }
