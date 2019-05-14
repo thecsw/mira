6ef9594a0e1294100f18cf5a7e2df7990123f409
JPadley18 <jacob5180@hotmail.co.uk>
2019-05-14 07:26:01 +0100

    Added new submission streaming function

--- mira/post.go
+++ mira/post.go
@@ -6,10 +6,10 @@ func (p *PostListing) GetChildren() []PostListingChild {
 	return p.Data.Children
 }
 
-func (plc *PostListingChild) GetAge() int64 {
-	return time.Now().UTC().Unix() - plc.Data.CreatedUtc
+func (plc *PostListingChild) GetAge() float64 {
+	return float64(time.Now().UTC().Unix()) - plc.Data.CreatedUtc
 }
 
-func (plc *PostListingChild) GetTimeCreated() int64 {
+func (plc *PostListingChild) GetTimeCreated() float64 {
 	return plc.Data.CreatedUtc
 }
--- mira/streaming.go
+++ mira/streaming.go
@@ -6,10 +6,12 @@ import (
 
 const (
 	CommentListInterval = 8
+	PostListInterval = 5
+	PostListSlice = 3
 )
 
 // c is the channel with all unread messages
-// stop is the channal to stop the stream. Do stop <- true to stop the loop
+// stop is the channel to stop the stream. Do stop <- true to stop the loop
 func (r *Reddit) StreamCommentReplies() (<-chan CommentListingDataChildren, chan bool) {
 	c := make(chan CommentListingDataChildren, 25)
 	stop := make(chan bool, 1)
@@ -26,6 +28,29 @@ func (r *Reddit) StreamCommentReplies() (<-chan CommentListingDataChildren, chan
 				}
 			}
 			time.Sleep(CommentListInterval * time.Second)
+			if <-stop {
+				return
+			}
+		}
+	}()
+	return c, stop
+}
+
+func (r *Reddit) StreamNewPosts(sr string) (<-chan Post, chan bool) {
+	c := make(chan Post)
+	stop := make(chan bool, 1)
+	go func(){
+		LastTime := time.Now().UTC().Unix()
+		for {
+			stop <- false
+			new, _ := r.GetSubredditPosts(sr, "new", PostListSlice)
+			for _, s := range new {
+				if s.GetTimeCreated() > float64(LastTime) {
+					c <- s.Data
+					LastTime = time.Now().UTC().Unix()
+				}
+			}
+			time.Sleep(PostListInterval * time.Second)
 			if <-stop {
 				return
 			}
