953d23b9a269a4920bc6b7def018d2072d059c28
Sandy <thecsw@ku.edu>
2019-05-15 19:20:04 -0500

    Added more methods for CommentListingDataChildren

--- commentlisting.go
+++ commentlisting.go
@@ -8,6 +8,26 @@ func (ldc *CommentListingDataChildren) GetId() string {
 	return ldc.Data.Name
 }
 
+func (ldc *CommentListingDataChildren) GetBody() string {
+	return ldc.Data.Body
+}
+
+func (ldc *CommentListingDataChildren) GetSubreddit() string {
+	return ldc.Data.Subreddit
+}
+
+func (ldc *CommentListingDataChildren) GetScore() float64 {
+	return ldc.Data.Score
+}
+
+func (ldc *CommentListingDataChildren) GetCreated() float64 {
+	return ldc.Data.Created
+}
+
+func (ldc *CommentListingDataChildren) GetAuthor() string {
+	return ldc.Data.Author
+}
+
 func (ldc *CommentListingDataChildren) IsComment() bool {
 	return ldc.Kind == "t1"
 }
