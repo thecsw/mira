7b2530e2cef10f54f0850f42227045aa26786ab6
Sandy <thecsw@ku.edu>
2019-05-26 09:37:19 -0500

    Added an example code for Reddit.GetSubmissionFromComment()

---
+++ examples/get_submission_from_comment.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/thecsw/mira"
+	"fmt"
+)
+
+func main() {
+	r, _ := mira.Init(mira.ReadCredsFromFile("login.conf"))
+	ch, stop := r.StreamNewPosts("popular")
+	r.Stream.PostListInterval = 2
+	i := 0
+	for {
+		post := <- ch
+		i++
+		fmt.Println("(", i, ") -->", post.GetTitle())
+		comment, _ := r.Comment(post.GetId(), "hello, world")
+		reply, _ := r.Reply(comment.GetId(), "you never reply")
+		fmt.Println(r.GetSubmissionFromComment(reply.GetId()))
+		if i == 1 {
+			stop <- true
+			break
+		}
+	}
+}
