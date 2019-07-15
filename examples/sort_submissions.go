d867c572ced4d7b87d92b1533c171db49f207c7a
Sandy <thecsw@ku.edu>
2019-07-15 00:00:51 -0500

    Fixed the error in examples with submissions slice

--- examples/sort_submissions.go
+++ examples/sort_submissions.go
@@ -13,7 +13,7 @@ func SortSubmissions() {
 	duration := "all"
 	subs, _ := r.GetSubredditPosts("subredditname", sort, duration, limit)
 
-	for _, v := range subs.GetChildren() {
+	for _, v := range subs {
 		fmt.Println("Submission Title: ", v.GetTitle())
 	}
 }
