c29158fcd23b83d3958a5a4b576fac2fa29a7a7b
Sandy <thecsw@ku.edu>
2019-12-07 22:19:40 -0600

    Safe data request from CommentWrap
    Signed-off-by: Sandy <thecsw@ku.edu>

--- models/comment.go
+++ models/comment.go
@@ -1,17 +1,23 @@
 package models
 
-func (c CommentWrap) GetId() string          { return c.Json.Data.Things[0].Data.Name }
-func (c CommentWrap) GetSubredditId() string { return c.Json.Data.Things[0].Data.SubredditId }
-func (c CommentWrap) GetParentId() string    { return c.Json.Data.Things[0].Data.ParentId }
-func (c CommentWrap) GetAuthor() string      { return c.Json.Data.Things[0].Data.Author }
-func (c CommentWrap) GetAuthorId() string    { return c.Json.Data.Things[0].Data.AuthorFullname }
-func (c CommentWrap) GetSubreddit() string   { return c.Json.Data.Things[0].Data.Subreddit }
-func (c CommentWrap) CreatedAt() float64     { return c.Json.Data.Things[0].Data.Created }
-func (c CommentWrap) GetBody() string        { return c.Json.Data.Things[0].Data.Body }
-func (c CommentWrap) GetScore() float64      { return c.Json.Data.Things[0].Data.Score }
-func (c CommentWrap) GetUps() float64        { return c.Json.Data.Things[0].Data.Ups }
-func (c CommentWrap) GetDowns() float64      { return c.Json.Data.Things[0].Data.Downs }
-func (c CommentWrap) IsSticky() bool         { return c.Json.Data.Things[0].Data.Stickied }
-func (c CommentWrap) IsRemoved() bool        { return c.Json.Data.Things[0].Data.Removed }
-func (c CommentWrap) IsApproved() bool       { return c.Json.Data.Things[0].Data.Approved }
-func (c CommentWrap) IsAuthor() bool         { return c.Json.Data.Things[0].Data.IsSubmitter }
+func (c CommentWrap) getThing() CommentJsonDataThing {
+	if len(c.Json.Data.Things) > 0 {
+		return c.Json.Data.Things[0]
+	}
+	return CommentJsonDataThing{}
+}
+func (c CommentWrap) GetId() string          { return c.getThing().Data.Name }
+func (c CommentWrap) GetSubredditId() string { return c.getThing().Data.SubredditId }
+func (c CommentWrap) GetParentId() string    { return c.getThing().Data.ParentId }
+func (c CommentWrap) GetAuthor() string      { return c.getThing().Data.Author }
+func (c CommentWrap) GetAuthorId() string    { return c.getThing().Data.AuthorFullname }
+func (c CommentWrap) GetSubreddit() string   { return c.getThing().Data.Subreddit }
+func (c CommentWrap) CreatedAt() float64     { return c.getThing().Data.Created }
+func (c CommentWrap) GetBody() string        { return c.getThing().Data.Body }
+func (c CommentWrap) GetScore() float64      { return c.getThing().Data.Score }
+func (c CommentWrap) GetUps() float64        { return c.getThing().Data.Ups }
+func (c CommentWrap) GetDowns() float64      { return c.getThing().Data.Downs }
+func (c CommentWrap) IsSticky() bool         { return c.getThing().Data.Stickied }
+func (c CommentWrap) IsRemoved() bool        { return c.getThing().Data.Removed }
+func (c CommentWrap) IsApproved() bool       { return c.getThing().Data.Approved }
+func (c CommentWrap) IsAuthor() bool         { return c.getThing().Data.IsSubmitter }
